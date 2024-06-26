cd047a19d305cd18866ce44fc91db695653fe238
mikek1337 <mikekegnlyew@gmail.com>
2024-06-26 21:36:18 +0300

    added google callback controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { LoginController } from './controllers/login.controller';
 import { LoginService } from './service/login.service';
 import { GoogleOAuthGuard } from 'src/google-oauth.guard';
 import { GoogleStrategy } from './google.strategy';
+import { GoogleCallbackController } from './controllers/google-callback.controller';
 
 @Module({
   imports: [],
-  controllers: [LoginController],
+  controllers: [LoginController, GoogleCallbackController],
   providers: [LoginService, GoogleStrategy],
 })
 export class AuthModule {}
