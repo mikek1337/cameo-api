ebdf4c4857e799379ff1b571b7fab3c70b7bc7f5
Mikek1337 <mikekegnlyew@gmail.com>
2024-06-28 16:04:58 +0300

    refactor: Update GoogleOAuthGuard to use 'prompt' option for Google OAuth

--- src/google-oauth.guard.ts
+++ src/google-oauth.guard.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class GoogleOAuthGuard extends AuthGuard('google') {
   constructor(private configService: ConfigService) {
     super({
-      accessType: 'offline',
+      prompt: 'select_account',
     });
   }
+
 }
